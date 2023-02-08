44a29bb3443f5ef49392576b5353acbbd91535c2
lroncevic <roncevicluka@outlook.com>
2023-02-08 21:20:31 +0100

    Setting Recipe Category for the H2 database

--- src/main/java/com/lukaroncevic/spring5recipesapp/domain/Recipe.java
+++ src/main/java/com/lukaroncevic/spring5recipesapp/domain/Recipe.java
@@ -18,8 +18,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //to do
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredientSet;
@@ -32,6 +30,11 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -127,5 +130,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
