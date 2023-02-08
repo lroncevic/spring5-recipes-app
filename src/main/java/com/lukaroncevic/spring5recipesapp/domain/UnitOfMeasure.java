3b5476017823495cab49d9f18b09f19ce178d772
lroncevic <roncevicluka@outlook.com>
2023-02-08 19:55:34 +0100

    Added Unit of Measure class & One to One relationship

---
+++ src/main/java/com/lukaroncevic/spring5recipesapp/domain/UnitOfMeasure.java
@@ -0,0 +1,31 @@
+package com.lukaroncevic.spring5recipesapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String description;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String desciption) {
+        this.description = desciption;
+    }
+}
